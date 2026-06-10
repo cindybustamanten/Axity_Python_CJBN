8e561ba8926727faf448e6573e746f801c10d9a7
vn0nv6j <vn0nv6j@m-FVFJD40Y1WG7.local>
2026-06-10 13:09:43 -0600

    Ajuste de carpetas

--- Fundamental_level/Entorno_y_herramientas/Laboratorios/json_processor/tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from json_processor.main import clasificar_edad
-
-
-def test_clasificacion():
-    assert clasificar_edad({"edad": 20}) == "joven"
-    assert clasificar_edad({"edad": 30}) == "adulto"
-    assert clasificar_edad({"edad": 40}) == "senior"
